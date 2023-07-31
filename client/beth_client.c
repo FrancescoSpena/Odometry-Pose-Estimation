ec2b61d1d98d6083f82e0966f57edd32972478aa
FrancescoSpena <francesco.spena4@gmail.com>
2023-07-31 14:13:26 +0200

    Fix

--- client/beth_client.c
+++ client/beth_client.c
@@ -3,6 +3,7 @@
 #include "serial.h"
 #include "print_packets.h"
 #include <stdio.h>
+#include <unistd.h>
 
 #define PACKET_BUFFER_SIZE 8
 
@@ -17,7 +18,15 @@ void BethClient_init(BethClient* c, char* dev, int speed){
 }
 
 PacketStatus BethClient_sendPacket(BethClient* c, PacketHeader* h){
-    return PacketHandler_sendPacket(&c->ph,h);
+    PacketHandler handler = c->ph;
+    PacketStatus ret = PacketHandler_sendPacket(&c->ph,h);
+    int fd = c->serial_fd;
+    uint8_t bytes = PacketHandler_txSize(&handler);
+    for(int i=0; i < bytes; i++){
+        uint8_t c = PacketHandler_popByte(&handler);
+        write(fd,&c,i);
+    }
+    return ret;
 }
 
 void recvFn(PacketHeader* recvp){
